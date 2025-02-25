3950fa43504ec07afdd0b6568b7dfad696d8712f
skdding3 <skdding3@gmail.com>
2025-02-26 06:15:16 +0900

    [feat] lib add

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold underline">Welcome to Next.js</h1>
+    </main>
+  );
+};
+
+export default Home;
